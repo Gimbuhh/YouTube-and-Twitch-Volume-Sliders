364a2736798cb32747873d25bd49661f417e07ff
Gimbuhh <210365920+Gimbuhh@users.noreply.github.com>
2026-06-28 15:00:23 +0200

    Release 2.6.2 icon and menu fixes (#16)

--- src/entries/youtube.user.js
+++ src/entries/youtube.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         YouTube Volume Slider
 // @namespace    https://github.com/Gimbuhh/YouTube-and-Twitch-Volume-Sliders
-// @version      2.6.1
+// @version      2.6.2
 // @description  Compact in-bar volume indicator that expands into a wide YouTube volume slider.
 // @author       Gimbuhh (Made using AI)
 // @icon         https://www.youtube.com/favicon.ico
 // @match        https://www.youtube.com/*
 // @updateURL    https://raw.githubusercontent.com/Gimbuhh/YouTube-and-Twitch-Volume-Sliders/main/dist/youtube-volume-slider.user.js
 // @downloadURL  https://raw.githubusercontent.com/Gimbuhh/YouTube-and-Twitch-Volume-Sliders/main/dist/youtube-volume-slider.user.js
-// @run-at       document-idle
+// @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
